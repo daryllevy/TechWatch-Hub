87b556c354eec3e91ae09812a1a98893c5e59df1
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-08-11 19:27:54 +0100

    connexion à MongoDB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
-const express = require("express");
+import express from "express";
+import mongoose, { connect } from "mongoose";
 const app = express();
 const port = 3000;
+const mongoURI =
+  "mongodb+srv://darylmvom01_db_user:azerty@cluster0.4elbwda.mongodb.net/?appName=Cluster0";
+
+mongoose.connect(mongoURI).then(() => console.log("Connecté à MongoDB"));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
