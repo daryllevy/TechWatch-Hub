05fd50f755422cd9a98b14a4f04894a8a96275f4
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-08-13 14:43:00 +0100

    création des routes + lectures des ressources

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
-import express from "express";
-import mongoose, { connect } from "mongoose";
-import dotenv from "dotenv";
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const resourcesRoutes = require("./routes/resourceRoutes");
 
 // Charger les variables d'environnement du fichier .env
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 const MONGO_URI = process.env.Mongo_URI;
 const PORT = process.env.port;
@@ -14,6 +16,8 @@ mongoose
   .connect(MONGO_URI)
   .then(() => console.log("Connecté à MongoDB"))
   .catch((err) => console.error("Erreur Mongoose : ", err));
+
+app.use("/api/resources", resourcesRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
