d9494188cc0109fb5665995420764a8e912b5387
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-08-14 13:01:33 +0100

    ajout de la méthode pour modifier une ressource

--- server/controllers/resourceController.js
+++ server/controllers/resourceController.js
@@ -12,13 +12,13 @@ exports.createResource = async (req, res) => {
 
 // Lister toutes les ressources
 exports.getAllResources = async (req, res) => {
-  const resources = await Resource.find();
+  const resources = await Resource.find().exec();
   res.json(resources);
 };
 
 exports.getResourceById = async (req, res) => {
   try {
-    const resource = await Resource.findById(req.params.id);
+    const resource = await Resource.findById(req.params.id).exec();
 
     if (!resource)
       return res.status(404).json({ error: "Ressource introuvable" });
@@ -28,5 +28,31 @@ exports.getResourceById = async (req, res) => {
     res
       .status(500)
       .json({ error: "L'id fournit n'est pas valide ou mal formé" });
+  }
+};
+
+// Modifier une ressource
+exports.updateResource = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updates = req.body;
+
+    const updatedResource = await Resource.findByIdAndUpdate(id, updates, {
+      new: true,
+      runValidators: true,
+    }).exec();
+
+    if (!updatedResource) {
+     return res.status(404).json({ error: "La ressource n'existe pas" });
+    }
+
+    res.status(200).json(updatedResource);
+  } catch (err) {
+    res
+      .status(400)
+      .json({
+        error:
+          "La requête envoyée est invalide : données manquantes ou mal formées",
+      });
   }
 };
