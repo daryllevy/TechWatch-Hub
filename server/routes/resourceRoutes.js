64fbbe30ff520b2cbc950cb70c2e5da378ccff5e
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-08-14 14:10:27 +0100

    close #16 : ajout de la route pour supprimer une ressource et Fin du CRUD Ressources

--- server/routes/resourceRoutes.js
+++ server/routes/resourceRoutes.js
@@ -6,5 +6,6 @@ router.post("/", resourceController.createResource);
 router.get("/", resourceController.getAllResources);
 router.get("/:id", resourceController.getResourceById);
 router.put("/:id", resourceController.updateResource);
+router.delete("/:id", resourceController.deleteResource);
 
 module.exports = router;
