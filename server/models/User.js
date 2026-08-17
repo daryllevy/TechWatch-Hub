22954252fcc2846484092b5b7abbee25d7bc574d
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-08-17 15:03:12 +0100

    création du schémaet du  modèle user

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next(); // donne le feu vert à Mongoose pour enregistrer
+  } catch (err) {
+    next(err); // on bloque l'enregistrement
+  }
+});
+
+module.exports = model("User", userSchema);
