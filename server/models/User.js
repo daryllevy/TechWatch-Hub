e6b326cefe2347101234f8166514d5ff2ec2b87b
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-08-18 15:02:43 +0100

    ajout de la casse sur la propriété email

--- server/models/User.js
+++ server/models/User.js
@@ -28,12 +28,8 @@ const userSchema = new Schema(
 userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-  } catch (err) {
-    throw err;
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 userSchema.methods.comparePassword = async function (userPassword) {
