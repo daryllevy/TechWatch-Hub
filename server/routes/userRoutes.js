f713a5b123a46725ad0f07011d0e88866ac30aae
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-08-20 12:49:47 +0100

    close #11 : ajout de la route de modification de profile

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 
-router.post("/register", userController.registerUser);
-router.post("/login", userController.loginUser);
+router.put("/me", authMiddleware, userController.updateProfile);
 
 module.exports = router;
