82879baf7043fc25badebb41a5c36c8f547781c9
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-08-21 14:54:36 +0100

    ajout de la propriété timestamp

--- server/models/Collection.js
+++ server/models/Collection.js
@@ -1,29 +1,32 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const collectionSchema = Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: String,
-  isPublic: {
-    type: Boolean,
-    default: true,
-  },
-  resources: [{ type: Schema.Types.ObjectId, ref: "Resource" }],
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  likesCount: { type: Number, default: 0 },
-  likedBy: [
-    {
+const collectionSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    isPublic: {
+      type: Boolean,
+      default: true,
+    },
+    resources: [{ type: Schema.Types.ObjectId, ref: "Resource" }],
+    userId: {
       type: Schema.Types.ObjectId,
       ref: "User",
     },
-  ],
-  viewCount: { type: Number, default: 0 },
-});
+    likesCount: { type: Number, default: 0 },
+    likedBy: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    viewCount: { type: Number, default: 0 },
+  },
+  { timestamp: true },
+);
 
 module.exports = model("Collection", collectionSchema);
