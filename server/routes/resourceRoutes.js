99f3d95f7b48dcec2f3fccc4b696395a3644fec8
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-08-21 14:52:10 +0100

    ajout du middleware d'authentification

--- server/routes/resourceRoutes.js
+++ server/routes/resourceRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const resourceController = require("../controllers/resourceController");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 
-router.post("/", resourceController.createResource);
+router.post("/", authMiddleware, resourceController.createResource);
 router.get("/", resourceController.getAllResources);
 router.get("/:id", resourceController.getResourceById);
 router.put("/:id", resourceController.updateResource);
