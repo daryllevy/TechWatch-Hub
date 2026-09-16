43693b11f1c2322d58d229974120a02ab1129f8c
Daryl Levy <222512535+daryllevy@users.noreply.github.com>
2026-09-16 14:37:01 +0100

    close #17 : implémentation de la fonctionnalité de changement du statut d'une ressource

--- server/controllers/resourceController.js
+++ server/controllers/resourceController.js
@@ -103,6 +103,35 @@ exports.updateResource = async (req, res) => {
   }
 };
 
+exports.updateResourceStatus = async (req, res) => {
+  try {
+    const resource = await Resource.findById(req.params.id).exec();
+    if (!resource) {
+      return res.status(404).json({ error: "La ressource n'existe pas" });
+    }
+
+    if (resource.userId.toString() !== req.user.toString()) {
+      return res.status(403).json({
+        error: "Accès refusé : cette ressource ne vous appartient pas.",
+      });
+    }
+
+    const { status } = req.body;
+    const updatedResource = await Resource.findByIdAndUpdate(
+      req.params.id,
+      { status },
+      { new: true, runValidators: true },
+    ).exec();
+
+    res.status(200).json(updatedResource);
+  } catch (err) {
+    res.status(400).json({
+      error:
+        "La requête envoyée est invalide : données manquantes ou mal formées",
+    });
+  }
+};
+
 exports.deleteResource = async (req, res) => {
   try {
     const id = req.params.id;
@@ -113,11 +142,9 @@ exports.deleteResource = async (req, res) => {
       return res.status(404).json({ error: "La ressource n'existe pas" });
 
     if (resource.userId.toString() !== req.user.toString()) {
-      return res
-        .status(403)
-        .json({
-          error: "Accès refusé : cette ressource ne vous appartient pas.",
-        });
+      return res.status(403).json({
+        error: "Accès refusé : cette ressource ne vous appartient pas.",
+      });
     }
 
     const deletedResource = await Resource.findByIdAndDelete(id);
--- server/routes/resourceRoutes.js
+++ server/routes/resourceRoutes.js
@@ -7,6 +7,11 @@ router.post("/", authMiddleware, resourceController.createResource);
 router.get("/", authMiddleware, resourceController.getAllResources);
 router.get("/:id", authMiddleware, resourceController.getResourceById);
 router.put("/:id", authMiddleware, resourceController.updateResource);
+router.put(
+  "/:id/status",
+  authMiddleware,
+  resourceController.updateResourceStatus,
+);
 router.delete("/:id", authMiddleware, resourceController.deleteResource);
 
 module.exports = router;
